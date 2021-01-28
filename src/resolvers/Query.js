bfe3592c9349c7c776f847c53ffae44b32c5a003
Rick Janusz <rickjanusz@gmail.com>
2021-01-27 18:45:38 -0600

    adding User

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,12 +1,14 @@
 const { forwardTo } = require('prisma-binding')
 
 const Query = {
+  items: forwardTo('db'),
   item: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
 
-  async items(parent, args, ctx, info) {
-    const items = await ctx.db.query.items()
-    return items
-  },
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items()
+  //   return items
+  // },
 
   // async item(parent, args, ctx, info) {
   //     const items = await ctx.db.query.items();
