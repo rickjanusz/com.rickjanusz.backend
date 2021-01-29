84771964722bdb821932a3ca1a65dd55b42a7a5f
Rick Janusz <rickjanusz@gmail.com>
2021-01-29 15:28:45 -0600

    user permissions, fix bug

--- src/index.js
+++ src/index.js
@@ -17,6 +17,21 @@ server.express.use((req, res, next) => {
   next()
 })
 
+//create a middleware that populates the user on each request
+
+server.express.use(async (req, res, next) => {
+  // 1. if they aren't logged in- skip this
+  if (!req.userId) return next()
+  const user = await db.query.user(
+    {
+      where: { id: req.userId },
+    },
+    '{id, permissions, email, name}'
+  )
+  req.user = user
+  next()
+})
+
 server.start(
   {
     cors: {
