a647165b6ac338333b11ed5e6ccc7df7cf6dc6ef
Rick Janusz <rickjanusz@gmail.com>
2021-02-02 10:39:03 -0600

    remove from cart functionality

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -232,6 +232,29 @@ const Mutations = {
       info
     )
   },
+  async removeFromCart(parent, args, ctx, info) {
+    // 1. find the cart item
+    const cartItem = await ctx.db.query.cartItem(
+      {
+        where: {
+          id: args.id,
+        },
+      },
+      `{id, user {id}}`
+    )
+    // 2. make sure we found the item
+    if (!cartItem) throw new Error('No cart item found!')
+    // 3. make sure they own the cart item
+    if (cartItem.user.id !== ctx.request.userId)
+      throw new Error('Cheatin Huhh?!')
+    // 4. delete the cart item
+    return ctx.db.mutation.deleteCartItem(
+      {
+        where: { id: args.id },
+      },
+      info
+    )
+  },
 }
 
 module.exports = Mutations
